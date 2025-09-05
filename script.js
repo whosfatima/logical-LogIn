9a7bf3c02f991c1cf015205fa8bbd5c04cab119c
maryam <fatimaisherewithyou@gmail.com>
2025-09-05 22:54:11 +0330

    ONGoing

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-const button= document.querySelector('button')
+const button = document.querySelector('button')
 
-const Name= document.getElementById('Name')
-const UserName= document.getElementById('UserName')
-const Email= document.getElementById('Email')
-const Password= document.getElementById('Password')
-const RePassword= document.getElementById('RePassword')
+const Name = document.getElementById('Name')
+const UserName = document.getElementById('UserName')
+const Email = document.getElementById('Email')
+const Password = document.getElementById('Password')
+const RePassword = document.getElementById('RePassword')
+const result = document.querySelector('.result')
 
-Name.addEventListener('input',() => {
-    if(Name.value ===''){
-        Name.classList.add('errorborder')
-        Name.classList.remove('succesBorder')
-    } else{
-        Name.classList.remove('errorborder')
-        Name.classList.add('succesBorder')
+// Name.addEventListener('input',() => {
+//     if(Name.value ===''){
+//         Name.classList.add('errorborder')
+//         Name.classList.remove('succesBorder')
+//     } else{
+//         Name.classList.remove('errorborder')
+//         Name.classList.add('succesBorder')
 
-    }
-})
+//     }
+// })
 button.addEventListener('click', (e) => {
-          Name.classList.remove('succesBorder')
+    let isvalid = true;
+    if (Name.value === '') {
+        Name.nextElementSibling.innerHTML = 'enter ur right name ...'
+        isvalid = false
+    } else {
+        Name.nextElementSibling.innerHTML = ''
+    }
     e.preventDefault();
-    
+    if (isvalid) {
+        result.style.display = 'block'
+        result.innerHTML = 'ok'
+    }
 })
