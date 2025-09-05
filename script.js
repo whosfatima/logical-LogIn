eb1ef0e46018c3bcc22e2ff8f200f93fd82cff67
maryam <fatimaisherewithyou@gmail.com>
2025-09-05 23:17:46 +0330

    we are going

--- script.js
+++ script.js
@@ -20,14 +20,44 @@ const result = document.querySelector('.result')
 button.addEventListener('click', (e) => {
     let isvalid = true;
     if (Name.value === '') {
-        Name.nextElementSibling.innerHTML = 'enter ur right name ...'
+        Name.nextElementSibling.innerHTML = 'enter the right name ...'
         isvalid = false
     } else {
         Name.nextElementSibling.innerHTML = ''
+    }
+    let userNamePatern = /^[a-zA-Z0-9_-]{3,9}$/
+    if (!userNamePatern.test(UserName.value)) {
+        UserName.nextElementSibling.innerHTML = 'Enter the right UserName...'
+        isvalid = false
+    } else {
+        UserName.nextElementSibling.innerHTML = ''
+    }
+
+    let emailPatern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!emailPatern.test(Email.value)) {
+        Email.nextElementSibling.innerHTML = 'Enter the right email'
+        isvalid = false
+    } else {
+        Email.nextElementSibling.innerHTML = ''
+    }
+    let passwordPatern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+    if (!passwordPatern.test(Password.value)) {
+        Password.nextElementSibling.innerHTML = 'pls enter the right password'
+        isvalid = false
+    } else {
+        Password.nextElementSibling.innerHTML = ''
+    }
+    if (RePassword.value !== Password.value) {
+        RePassword.nextElementSibling.innerHTML = 'they are not match...'
+        isvalid = false
+    } else {
+        RePassword.nextElementSibling.innerHTML = ''
     }
     e.preventDefault();
     if (isvalid) {
         result.style.display = 'block'
         result.innerHTML = 'ok'
+    } else {
+        result.style.display = 'none'
     }
 })
